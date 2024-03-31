ff39147b17dcf8dbfbc864351e8b918cd7e1ffbe
rusgyn <rusgynmagsanay@gmail.com>
2024-03-31 15:41:03 -0400

    Remove unused code

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const { getUserNotes } = require('../db/queries/users');
 
 router.get('/', (req, res) => {
   const user = req.session.user;
@@ -12,43 +11,4 @@ router.get('/', (req, res) => {
   res.render('notes', { user });
 });
 
-// GET request to render the login form
-// router.get('/:id', (req, res) => {
-//   const { id } = req.params;
-//   const email = req.session.user.email;
-//   getUserNotes(id)
-//     .then(notes => {
-//       res.render('notes', { notes, email });
-//     })
-//     .catch(err => {
-//       res
-//         .status(500)
-//         .json({ error: err.message });
-//     });
-// });
-
-// router.post('/', (req, res) => {
-//   console.log("Request -->> ", req.body);
-//   const text = req.body.text;
-//   const userId = req.session.user.id;
-//   let category;
-
-//   if (text.includes('watch')) {
-//     category = 'Watch';
-//   } else if (text.includes('eat')) {
-//     category = 'Eat';;
-//   } else if (text.includes('read')) {
-//     category = 'Read';;
-//   } else if (text.includes('buy')) {
-//     category = 'Buy';;
-//   }
-
-//   addNote(userId, text, category)
-//   .then(() => {
-//     res.redirect(`/notes/${userId}`);
-//   })
-// });
-
-
 module.exports = router;
-
